145d6aa1195c3af015d2ff78de4145e11b0497c6
pearcedom <dominic.pearce@ed.ac.uk>
2016-08-18 12:26:11 +0100

    scrape function + GW2 scrape

---
+++ 16-17/lib/scrapeFF.R
@@ -0,0 +1,16 @@
+
+scrapeFF <- function(x){
+    library(jsonlite)
+    library(tibble)
+    library(magrittr)
+
+    ff.list <- fromJSON("https://fantasy.premierleague.com/drf/bootstrap-static")
+    ff.dfr_base <- as_tibble(ff.list$elements)
+    ff.dfr_base$element_type <- ifelse(ff.dfr_base$element_type == 1, "GK",
+                                    ifelse(ff.dfr_base$element_type == 2, "DEF",
+                                        ifelse(ff.dfr_base$element_type == 3, "MID", "FWD")))
+
+    teams_dfr <- tibble(team_code = c(3, 91, 90, 8, 31, 20, 11, 43, 88, 13, 14, 110, 1, 25, 56, 80, 21, 6, 57, 35), team = c("Arsenal", "Bournemouth", "Burnley", "Chelsea", "Crystal Palace", "Southampton", "Everton", "Man City", "Hull", "Leicester", "Liverpool", "Stoke", "Man Utd", "Middlesbrough", "Sunderland", "Swansea", "West Ham", "Tottenham", "Watford", "West Brom"))
+
+    merge(ff.dfr_base, teams_dfr, by = "team_code") %>% as_tibble()
+}
